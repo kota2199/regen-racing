d83c282f1a6059d4365a184848d25ec85f87476e
結城康太 <kota.2199@icloud.com>
2025-01-21 19:42:07 +0900

    result scene

--- Assets/Scripts/04_Game/00_System/ResultManager.cs
+++ Assets/Scripts/04_Game/00_System/ResultManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using DG.Tweening;
 
 public class ResultManager : MonoBehaviour
 {
@@ -11,6 +12,10 @@ public class ResultManager : MonoBehaviour
 
     [SerializeField]
     private List<GameObject> Uis;
+
+    [SerializeField]
+    private RectTransform[] positions;
+
 
     [SerializeField]
     private string[] futureOptions;
@@ -27,12 +32,33 @@ public class ResultManager : MonoBehaviour
     [SerializeField]
     private FadeInOut fadeInOut;
     [SerializeField]
-    private AudioClip decide;
+    private AudioClip time, eco ,total, afterTotal, decide;
+
+    [SerializeField]
+    private float displayWaitTime = 2f;
+
+    [SerializeField]
+    private float countDuration = 1f;
+
+    private bool isDisplayFinished = false;
+
+    private AudioSource audioSource;
+
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
 
     private void Start()
     {
         raceDate.UpdateCarPositionsByTime();
+        ResetPosition();
+        StartCoroutine(DisplayResult());
+    }
 
+    private IEnumerator DisplayResult()
+    {
+        //Display Image & Mode
         for (int i = 0; i < raceDate.cars.Count; i++)
         {
             Text modeText = Uis[i].transform.Find("t_Mode").GetComponent<Text>();
@@ -49,39 +75,93 @@ public class ResultManager : MonoBehaviour
             }
 
             Uis[i].transform.Find("im_Car").GetComponent<Image>().sprite = carImage[raceDate.cars[i].colorIndex];
-            Uis[i].transform.Find("t_Pos").GetComponent<Text>().text = raceDate.cars[i].Position.ToString() + MakeOrdinalNumber(raceDate.cars[i].Position.ToString());
-            Uis[i].transform.Find("t_Time").GetComponent<Text>().text = FormatToMMSSSSS(raceDate.cars[i].time);
-            Uis[i].transform.Find("t_Eco").GetComponent<Text>().text = ecoPoint[raceDate.cars[i].choiceIndex].ToString();
-            Uis[i].transform.Find("t_Total").GetComponent<Text>().text = FormatToMMSSSSS(TotalTime(raceDate.cars[i].choiceIndex,raceDate.cars[i].time));
         }
-    }
 
-    private string MakeOrdinalNumber(string num)
-    {
-        switch (num)
+        yield return new WaitForSeconds(displayWaitTime + 1);
+
+        //Display Time
+        for (int i = 0; i < raceDate.cars.Count; i++)
         {
-            case "1":
-                return "st";
+            if (raceDate.cars[i].isFinished)
+            {
+                StartCoroutine(CountUp(Uis[i].transform.Find("t_Time").GetComponent<Text>(), 0, raceDate.cars[i].time));
+                //Uis[i].transform.Find("t_Time").GetComponent<Text>().text = FormatToMMSSSSS(raceDate.cars[i].time);
+            }
+            else
+            {
+                Uis[i].transform.Find("t_Time").GetComponent<Text>().text = "DNF";
+            }
 
-            case "2":
-                return "nd";
-
-            case "3":
-                return "rd";
-
-            default:
-                return "th";
+            audioSource.PlayOneShot(time);
         }
+
+        yield return new WaitForSeconds(displayWaitTime);
+
+        //Display Eco
+        for (int i = 0; i < raceDate.cars.Count; i++)
+        {
+            if (raceDate.cars[i].isFinished)
+            {
+                StartCoroutine(CountUp(Uis[i].transform.Find("t_Eco").GetComponent<Text>(), 0, ecoPoint[raceDate.cars[i].choiceIndex]));
+                //Uis[i].transform.Find("t_Eco").GetComponent<Text>().text = ecoPoint[raceDate.cars[i].choiceIndex].ToString();
+            }
+            else
+            {
+                Uis[i].transform.Find("t_Eco").GetComponent<Text>().text = "DNF";
+            }
+
+            audioSource.PlayOneShot(eco);
+        }
+        yield return new WaitForSeconds(displayWaitTime);
+
+        //Display Total
+        for (int i = 0; i < raceDate.cars.Count; i++)
+        {
+            if (raceDate.cars[i].isFinished)
+            {
+                raceDate.cars[i].totalTime = TotalTime(raceDate.cars[i].choiceIndex, raceDate.cars[i].time);
+
+                StartCoroutine(CountUp(Uis[i].transform.Find("t_Total").GetComponent<Text>(), 0, raceDate.cars[i].totalTime));
+                //Uis[i].transform.Find("t_Total").GetComponent<Text>().text = FormatToMMSSSSS(raceDate.cars[i].totalTime);
+            }
+            else
+            {
+                Uis[i].transform.Find("t_Total").GetComponent<Text>().text = "DNF";
+            }
+            audioSource.PlayOneShot(total);
+        }
+
+        raceDate.UpdateCarPositionsByTotalTime();
+
+        yield return new WaitForSeconds(1f);
+        ResetPosition();
+        audioSource.PlayOneShot(afterTotal);
+
+        isDisplayFinished = true;
     }
+
     private string FormatToMMSSSSS(float totalSeconds)
     {
-        // 分を計算
-        int minutes = (int)(totalSeconds / 60);
-        // 秒を計算
-        float seconds = totalSeconds % 60;
+        if(totalSeconds < 0)
+        {
+            // 分を計算
+            int minutes = -(int)(totalSeconds / 60);
+            // 秒を計算
+            float seconds = Mathf.Abs(totalSeconds) % 60;
 
-        // MM:SS.SSS形式の文字列を返す
-        return $"{minutes}:{seconds:00.000}";
+            // MM:SS.SSS形式の文字列を返す
+            return $"{minutes}:{seconds:00.000}";
+        }
+        else
+        {
+            // 分を計算
+            int minutes = (int)(totalSeconds / 60);
+            // 秒を計算
+            float seconds = totalSeconds % 60;
+
+            // MM:SS.SSS形式の文字列を返す
+            return $"{minutes}:{seconds:00.000}";
+        }
     }
 
     private float TotalTime(int index, float time)
@@ -104,11 +184,38 @@ public class ResultManager : MonoBehaviour
         }
     }
 
+    private void ResetPosition()
+    {
+        for (int i = 0; i < raceDate.cars.Count; i++)
+        {
+            Uis[i].GetComponent<RectTransform>().DOAnchorPos(positions[raceDate.cars[i].Position - 1].anchoredPosition, 0.5f).SetEase(Ease.InSine);
+        }
+    }
+
+    private IEnumerator CountUp(Text timeText, float startNum, float targetNum)
+    {
+        float num = startNum;
+        float elapsedTime = 0;
+        while (true)
+        {
+            elapsedTime += Time.deltaTime;
+            num = Mathf.FloorToInt(Mathf.Lerp(0, targetNum, elapsedTime / countDuration));
+            timeText.text = FormatToMMSSSSS(num);
+
+            if (countDuration <= elapsedTime)
+            {
+                yield break;
+            }
+
+            yield return null;
+        }
+    }
+
     private void Update()
     {
-        if (Input.GetButtonDown("Fire_0") || Input.GetKeyDown(KeyCode.C))
+        if (isDisplayFinished && Input.GetButtonDown("Fire_0") || Input.GetKeyDown(KeyCode.C))
         {
-            GetComponent<AudioSource>().PlayOneShot(decide);
+            audioSource.PlayOneShot(decide);
             fadeInOut.FadeOut();
             Invoke("ToTitle", 2f);
         }
--- Assets/Scripts/04_Game/99_Data/RaceData.cs
+++ Assets/Scripts/04_Game/99_Data/RaceData.cs
@@ -43,34 +43,11 @@ public class RaceData : ScriptableObject
         return cars.Find(car => car.CarName == carName);
     }
 
-    /*public int GetCarLap(string carName)
-    {
-        CarInfo car = GetCarInfo(carName);
-        return car.Lap;
-    }
-
-    public int GetCarPassedPointNumber(string carName)
-    {
-        CarInfo car = GetCarInfo(carName);
-        return car.PassedPoint;
-    }
-
-    public void UpdateCarInfo(string carName, int lap, int passedPoint, float time)
-    {
-        CarInfo car = GetCarInfo(carName);
-        if (car != null)
-        {
-            car.Lap = lap;
-            car.PassedPoint = passedPoint;
-            car.time = time;
-        }
-    }
-*/
     public void UpdateCarPositionsByTime()
     {
         // CarInfoリストをtimeの小さい順にソート
         List<CarInfo> sortedCars = new List<CarInfo>(cars);
-        sortedCars.Sort((carA, carB) => carA.Position.CompareTo(carB.Position));
+        sortedCars.Sort((carA, carB) => carA.time.CompareTo(carB.time));
         sortedCars.Sort((carA, carB) => carB.isFinished.CompareTo(carA.isFinished));
 
         // ソートされたリストの順番に基づいてPositionを設定
@@ -84,6 +61,26 @@ public class RaceData : ScriptableObject
         {
             cars[i] = sortedCars[i];
         }
+    }
+
+    public void UpdateCarPositionsByTotalTime()
+    {
+        // CarInfoリストをtimeの小さい順にソート
+        List<CarInfo> sortedCars = new List<CarInfo>(cars);
+        sortedCars.Sort((carA, carB) => carA.totalTime.CompareTo(carB.totalTime));
+        sortedCars.Sort((carA, carB) => carB.isFinished.CompareTo(carA.isFinished));
+
+        // ソートされたリストの順番に基づいてPositionを設定
+        for (int i = 0; i < sortedCars.Count; i++)
+        {
+            sortedCars[i].Position = i + 1; // Positionは1から始まる
+        }
+
+        // ソートされたリストを元のcarsリストに反映
+        //for (int i = 0; i < cars.Count; i++)
+        //{
+        //    cars[i] = sortedCars[i];
+        //}
     }
 
     public void UpdateCarColorInfo(string name, int colorIndex)
@@ -115,33 +112,33 @@ public class RaceData : ScriptableObject
         }
     }
 
-    public void UpdateFinishStatus(string name, float totalTime)
-    {
-        CarInfo car = GetCarInfo(name);
-        if (car != null)
-        {
-            car.isFinished = true;
-            car.totalTime = totalTime;
-            switch (car.choiceIndex)
-            {
-                case 0:
-                    car.ecoPoint = -10.00f;
-                    car.totalTime = car.time + car.ecoPoint;
-                    break;
-                case 1:
-                    car.ecoPoint = 0.00f;
-                    car.totalTime = car.time + car.ecoPoint;
-                    break;
-                case 2:
-                    car.ecoPoint = 10.00f;
-                    car.totalTime = car.time + car.ecoPoint;
-                    break;
-                default:
-                    Debug.LogError("Out of Index");
-                    break;
-            }
+    //public void UpdateFinishStatus(string name, float totalTime)
+    //{
+    //    CarInfo car = GetCarInfo(name);
+    //    if (car != null)
+    //    {
+    //        car.isFinished = true;
+    //        car.totalTime = totalTime;
+    //        switch (car.choiceIndex)
+    //        {
+    //            case 0:
+    //                car.ecoPoint = -10.00f;
+    //                car.totalTime = car.time + car.ecoPoint;
+    //                break;
+    //            case 1:
+    //                car.ecoPoint = 0.00f;
+    //                car.totalTime = car.time + car.ecoPoint;
+    //                break;
+    //            case 2:
+    //                car.ecoPoint = 10.00f;
+    //                car.totalTime = car.time + car.ecoPoint;
+    //                break;
+    //            default:
+    //                Debug.LogError("Out of Index");
+    //                break;
+    //        }
 
-            //UpdateCarPositionsByTime();
-        }
-    }
+    //        //UpdateCarPositionsByTime();
+    //    }
+    //}
 }
